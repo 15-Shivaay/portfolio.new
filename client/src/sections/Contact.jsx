b4ebc9aca9503dbe2a92d5bea1a7629f90beed40
Shivam Singh <shivamumeshsingh99@gmail.com>
2026-05-12 01:39:28 +0530

    updatng

--- client/src/sections/Contact.jsx
+++ client/src/sections/Contact.jsx
@@ -14,8 +14,7 @@ export default function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-
-    console.log(import.meta.env.VITE_API_URL);
+    
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/api/contact`, {
         method: 'POST',
--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://portfolio-chi-navy-81.vercel.app/'
+  ],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 
 
